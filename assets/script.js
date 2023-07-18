ed0c5576e1add24591bc825c773e7f79d14b1d36
Kejvin Pashaj <kevin.952011@gmail.com>
2023-07-18 15:43:26 +0100

    script code refractor

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,9 @@
 const resultContainer = document.querySelector(".result-container");
-const resultText = document.querySelector("#result-text");
+// const resultText = document.querySelector("#result-text");
 const textInput = document.querySelector("#text-input");
 const buttonCheck = document.querySelector("#button-check");
-console.log(textInput.textContent)
 
-// Add eventListener for buttonCheck
-buttonCheck.addEventListener("click", () => {
-    // Check if textInput is palindrome, return resultText "Is palindrome"
-    if(!palindrome(textInput.textContent)){
-        resultText.innerText += "Not palindrome";
-    } else {
-        // else return resultText "Not palindrome"
-        resultText.innerText += "Is palindrome";
-    }
-})
-
-
-
+// Function to check for palindrome strings
 function palindrome(str) {
     // Remove any punctuation, spaces and symbols
     let newStr = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
@@ -29,3 +16,23 @@ function palindrome(str) {
         return true;
     }
 }
+
+// Check if input string is palindrome when button is clicked
+buttonCheck.addEventListener("click", () => {
+    // Create a new <h4> element with result text, and append to parent container
+    // Make sure to append new element only if it doesn't already exist
+    let result = document.querySelector("#result-text");
+    if(!result){
+        result = document.createElement("h4");
+        result.setAttribute("id", "result-text");
+        resultContainer.appendChild(result);
+    }
+
+    // Check if textInput is palindrome, return resultText "Is palindrome"
+    if(palindrome(textInput.value)){
+        result.innerText = "Is palindrome";
+    } else {
+        // else return resultText "Not palindrome"
+        result.innerText = "Not palindrome";
+    }
+})
