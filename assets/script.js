28dcdf5e76bf043f7e93af7da4fce34349296e4b
Kejvin Pashaj <kevin.952011@gmail.com>
2023-07-18 12:32:32 +0100

    added elements variables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
+const resultContainer = document.querySelector(".result-container");
+const resultText = document.querySelector("#result-text");
+const textInput = document.querySelector("#text-input");
+const buttonCheck = document.querySelector("#button-check");
+
+// Add eventListener for buttonCheck
+    // Check if textInput is palindrome, return resultText "Is palidndrome"
+    // else return resultText "Not palindrome"
+
+
 function palindrome(str) {
     // Remove any punctuation, spaces and symbols
     let newStr = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
